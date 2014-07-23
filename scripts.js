dfbd0cccc9fdccc9731cb5d01ea2cf65327b7a19
tag1 <rlthompsonjr@gmail.com>
2014-07-23 10:45:09 -0400

    functional
    fully functional, added dom insertion and fizz buzz replacements.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
+$(document).ready(function () {
 
-	for (var number = 1; number < 101; number++) {
-	console.log(number);
+	for (number = 1; number < 101; number++) {
 	if (number % 15 === 0) {
-		console.log("fizzbuzzy");
+		$("p").append("fizzbuzzy" + "<br><br>");
 	} 
-	else
-		if (number % 5 === 0) {
-		console.log("buzz");
-		}
 		else
-			if (number % 3 === 0) {
-				console.log("fizz")
-			};
-};	
-
+			if (number % 5 === 0) {
+			$("p").append("buzz" + "<br><br>");
+			}
+			else
+				if (number % 3 === 0) {
+					$("p").append("fizz" + "<br><br>");
+				}
+				else
+					 $("p").append(number + "<br><br>");
+		}
+	}	
+);
 	
 
