0f4da4cbdd4bcd1c3720e1f7435b668058b24b09
tag1 <rlthompsonjr@gmail.com>
2014-07-24 09:22:03 -0400

    draft 3 / continue
    started adding user input
    added basic css file

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+
+
+	$("button").click(function() {
+		event.preventDefault();
+		var number = $("input").val();
+		$(".stuff").append("<p>" + number+ "</p>");
+		$("input").val(" ");
+	}
+
+)}
+);
+
+/*
+$(document).ready(function () {
+
 	for (number = 1; number < 101; number++) {
 	if (number % 15 === 0) {
 		$("p").append("fizzbuzzy" + "<br><br>");
@@ -17,5 +32,30 @@ $(document).ready(function () {
 		}
 	}	
 );
-	
+*/	
+/*
+var enterBox = function() {
+	var x = $("input").val(+"");
+		if (x === ) {};
+}
+
+var enterResponse = function(integer) {
+	for (var number = 1; number != integer; number++)
+		if (number % 15 === 0) {
+		$("p").append("fizzbuzzy" + "<br><br>");
+	} 
+		else
+			if (number % 5 === 0) {
+			$("p").append("buzz" + "<br><br>");
+			}
+			else
+				if (number % 3 === 0) {
+					$("p").append("fizz" + "<br><br>");
+				}
+				else
+					 $("p").append(number + "<br><br>");
+	};
+}
+*/
+
 
